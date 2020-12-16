f3c1e34fe526680e1ef23d81d12fe2a21e77cf0c
Beomsookwak <71259069+Beomsookwak@users.noreply.github.com>
2020-12-16 15:02:02 +0900

    Update relay.py
    relay.py 코드 주석 추가

--- DoorLock/relay.py
+++ DoorLock/relay.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setmode(GPIO.BCM) //GPIO의 핀 번호를 BCM모드로 참조
+GPIO.setup(18,GPIO.OUT) //18 GPIO 핀의 출력 설정
 
-GPIO.output(18,True)
-print "power_in"
-time.sleep(2)
+GPIO.output(18,True) // 출력모드로 설정된 핀에 True값을 할당
+print "power_in" 
+time.sleep(2) //2초동안 정지
 
-GPIO.cleanup()
+GPIO.cleanup() // 핀 설정을 초기화
